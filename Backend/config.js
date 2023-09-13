bdbcc04629c1e92257f5366e55b486b3547e5392
Katiuska Marin <kmmarin@espol.edu.ec>
2023-09-13 10:14:11 -0500

    se cambio constructor para recibir credenciales de db

--- Backend/config.js
+++ Backend/config.js
@@ -2,18 +2,22 @@ require('dotenv').config()
 
 const { Sequelize } = require('sequelize');
 
-const sicuel = new Sequelize(process.env.DATABASE_URL);
+//const sicuel = new Sequelize(process.env.DATABASE_URL);
 
-// const sicuel = new Sequelize( process.env.DATABASE, process.env.USER, process.env.PASSWORD,
-//     {
-//         host: process.env.HOST,
-//         dialect: 'postgres',
-//         port: process.env.PORT_DB,
-//         logging: false,
-//         dialectOptions:{
-//             statement_timeout: process.env.QUERY_TIMEOUT
-//         }
-//     }
-// ) 
+const sicuel = new Sequelize( process.env.DATABASE, process.env.USER, process.env.PASSWORD,
+    {
+        host: process.env.HOST,
+        dialect: 'postgres',
+        port: process.env.PORT_DB,
+        logging: false,
+        dialectOptions:{
+            statement_timeout: process.env.QUERY_TIMEOUT,
+            ssl: {
+              require: true, // This will help you. But you will see nwe error
+              rejectUnauthorized: false // This line will fix new error
+            }
+        },
+    }
+) 
 
 module.exports =  { sicuel }; 
