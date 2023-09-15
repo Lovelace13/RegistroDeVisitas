ae2904f1bbf9e8565f170c8112195c1be26f5976
Katiuska Marin <kmmarin@espol.edu.ec>
2023-09-15 09:03:58 -0500

    se reverso cambio

--- frontend/src/components/Visita.jsx
+++ frontend/src/components/Visita.jsx
@@ -1,43 +1,34 @@
 
+
 import { Card, Row } from 'react-bootstrap';
 import {ListGroup, Button }from 'react-bootstrap';
 import { useState, useEffect } from 'react';
-import PaginacionV from "../components/paginacionVisita";
 
 const Visita = ({ visit }) => {
-  const [total, setTotal] = useState(0)
-  const [listVisit, setListVisit] = useState([])
-  const [currentPage, setCurrentPage] = useState(1);
-  const [visitPerPage] = useState(2);
 
-  const paginate = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  };
-
-  const indexOfLastVisit = currentPage * visitPerPage;
-  const indexOfFirstVisit = indexOfLastVisit - visitPerPage;
-  const currentVisits = visit?.slice(indexOfFirstVisit, indexOfLastVisit);
-
-
+  
   return (
     <>
-      <Row className="justify-content-center">
-        { visit?.map( (visita) => (    
-            <Card style={{ width: '18rem' }}>
-              <ListGroup key={visita.idvisita} variant="flush">
-                <ListGroup.Item>fecha ingreso: {visita.fechaingreso} </ListGroup.Item>
-                <ListGroup.Item>Hora: {visita.hora}</ListGroup.Item>
-                <ListGroup.Item>Nombre: {visita.nombre} </ListGroup.Item>
-                <ListGroup.Item>Cedula: {visita.cedula}</ListGroup.Item>
-                <ListGroup.Item>Motivo: {visita.motivo} </ListGroup.Item>
-                <ListGroup.Item>Dpto visitado: {visita.departamento}</ListGroup.Item>
-                <ListGroup.Item>Estado Visita: {visita.estado} </ListGroup.Item>
-                <ListGroup.Item>Novedad: {visita.novedad}</ListGroup.Item> 
-              </ListGroup>
-            </Card>      
+    <Row className="justify-content-center">
+    < Card style={{ width: '18rem' }}>
+        { visit?.map( (visita) => (
+          <ListGroup key={visita.idvisita} variant="flush">
+            <ListGroup.Item>fecha ingreso: {visita.fechaingreso} </ListGroup.Item>
+            <ListGroup.Item>Hora: {visita.hora}</ListGroup.Item>
+            <ListGroup.Item>Nombre: {visita.nombre} </ListGroup.Item>
+            <ListGroup.Item>Cedula: {visita.cedula}</ListGroup.Item>
+            <ListGroup.Item>Motivo: {visita.motivo} </ListGroup.Item>
+            <ListGroup.Item>Dpto visitado: {visita.departamento}</ListGroup.Item>
+            <ListGroup.Item>Estado Visita: {visita.estado} </ListGroup.Item>
+            <ListGroup.Item>Novedad: {visita.novedad}</ListGroup.Item>
+  
+          </ListGroup>
         ))}
-      </Row>
-      <PaginacionV visitasPorPag={visitPerPage} totalVisitas={visit?.length} paginate={paginate}/>
+      </Card>
+    </Row>
+      
+      
+      {/* <Button variant="info" onClick={()=>cambiarNovedad()}>Actualizar novedad</Button>{' '} */}
     </>
   )
 
